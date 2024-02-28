02782ad5bd12516aee7a2c02cf58c81830880c5b
Yuri2224 <yuri224@gmail.com>
2024-02-28 11:28:18 -0300

    aulao

--- simple/script.js
+++ simple/script.js
@@ -1,28 +1,50 @@
 function calcular(){
+
     var n1 = document.getElementById("num1").valueAsNumber;
     var n2 = parseInt(document.getElementById("num2").value);
     var res = document.getElementById("res")
 
-if(isNaN(n1)&& isNaN(n2)){
+    if(isNaN(n1)&& isNaN(n2)){
     alert("digite números válidos")
     return;
+    }
+    res.textContent= n1 + n2
+    // res.textContent = n1 + n2  
 }
-
-        res.textContent= n1 + n2
-        // res.textContent = n1 + n2
-}
-
-function media(){
-    var n1 =  document.getElementById("nota1").valueAsNumber;
-    var n2 =  document.getElementById("nota2").valueAsNumber;
-    var n3 =  document.getElementById("nota3").valueAsNumber;
-    var res =document.getElementById("resposta");
-    res.textContent = ((n1 + n2 + n3) / 3).toFixed(2)
-
-    if(res > 7){
-        res.style.color = "green";
+        function media(){
+            var n1 =  document.getElementById("nota1").valueAsNumber;
+            var n2 =  document.getElementById("nota2").valueAsNumber;
+            var n3 =  document.getElementById("nota3").valueAsNumber;
+            var mediatexto =document.getElementById("resposta");
+            var res = (n1 + n2 + n3) / 3;
+    if(res >= 7){
+        mediatexto.innerHTML = res.toFixed(2)
+        mediatexto.style.color = "green"
 
     }else{
-        res.style.color = "red";
+        mediatexto.innerHTML = res.toFixed(2)
+        mediatexto.style.color = "red"
     }
 }
+
+  function contar(){
+    const num = parseInt($("#numeroInput").val());
+    // var num =  document.getElementById("numeroInput").valueAsNumber;
+
+    console.log(num)
+
+    if(isNaN(num)){
+        $('#result').text("por favor, informe um número válido");
+        return;
+    }
+    var resultadoStr = ""
+    if(num <= 10000){
+        for(var cont = 1; cont <= num; cont++){ 
+            resultadoStr = resultadoStr + cont + "<br>";
+        }
+        $("#result").html(resultadoStr)
+    }else{
+        $("#result").text("Vai travar se você digitar este numero");
+    }
+
+    }
